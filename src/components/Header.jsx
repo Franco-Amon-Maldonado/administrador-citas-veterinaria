9e20420fae040fc4be63c8c61dfdd1050a74a4d7
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-01-28 00:29:27 -0300

    28/01/2023
    -Cambio de rutas del logo y icon
    -Delete svg vite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
  function Header(){
    return (
      <div>
-          <img src="/src/assets/logo.png" alt="logo" className="w-36 drop-shadow"/>
+          <img src="./src/assets/img/logo.png" alt="logo" className="w-36 drop-shadow"/>
 
-          <h1 className="text-center text-5xl font-black md:w-2/3 mx-auto">Siguimiento pacientes <span className="text-green-800">Veterinaria</span></h1>
+          <h1 className="text-3xl mt-9 text-center md:text-5xl md:mt-0 font-black md:w-2/3 mx-auto">Siguimiento pacientes <span className="text-green-800">Veterinaria</span></h1>
 
 
      </div>

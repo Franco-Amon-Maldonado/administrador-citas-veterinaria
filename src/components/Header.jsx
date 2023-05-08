5053686ff7576ab21c9f9cd35952e64bb96e0eba
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-05-08 00:47:38 -0300

    08/05/2023
    --

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
  function Header(){
    return (
      <div>
-          <img src="./assets/img/logo.png" alt="logo" className="w-36 drop-shadow"/>
+          <img src="../../public/img/logo.png" alt="logo" className="w-36 drop-shadow"/>
 
           <h1 className="text-3xl mt-9 text-center md:text-5xl md:mt-0 font-black md:w-2/3 mx-auto">Siguimiento pacientes <span className="text-green-800">Veterinaria</span></h1>
 
